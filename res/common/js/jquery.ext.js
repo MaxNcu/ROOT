286db1539f942504d459a74cb3d28848d8ddbc86
glitch-ss <547076180@qq.com>
2017-08-04 22:59:38 +0800

    mobile test

--- res/common/js/jquery.ext.js
+++ res/common/js/jquery.ext.js
@@ -1,4 +1,4 @@
-$.metadata.setType("attr", "vld");
+﻿$.metadata.setType("attr", "vld");
 //允许传统传递数组参数
 jQuery.ajaxSettings.traditional = true;
 $.validator.AlertError = {
@@ -18,37 +18,37 @@ $.validator.addMethod("username", function(value) {
 	if(value.length==0) {return true;}
 	var p = /^[0-9a-zA-Z\u4e00-\u9fa5\.\-@_]+$/;
 	return p.exec(value) ? true : false;
-}, "请只输入字母，数字，汉字和_ '，' '，“@”");
+}, lang['qingzhishuru']);
 $.validator.addMethod("path", function(value) {
 	if(value.length==0) {return true;}
 	var p = /^[0-9a-zA-Z]+$/;
 	return p.exec(value) ? true : false;
-}, "请只输入字母和数字");
+}, lang['qingzhishuruzimuheshuzi']);
 $.extend($.validator.messages, {
-	required : "该项为必填项",
-	remote : "请修正该字段",
-	email : "请输入正确格式的电子邮件",
-	url : "请输入合法的网址",
-	date : "请输入合法的日期",
-	dateISO : "请输入合法的日期 ",
-	number : "请输入合法的数字",
-	digits : "只能输入整数",
+	required : lang['gaixiangweibitian'],
+	remote : lang['qingxiuzhenggaiziduan'],
+	email : lang['qingshuruzhengquegeshidedianziyoujian'],
+	url : lang['qingshuruhefadewangzhi'],
+	date : lang['qingshuruhefaderiqi'],
+	dateISO : lang['qingshuruhefaderiqi'],
+	number : lang['qingshuruhefadeshuzi'],
+	digits : lang['zhinengshuruzhengshu'],
 	creditcard : "请输入合法的信用卡号",
 	equalTo : "请再次输入相同的值",
 	accept : "请输入拥有合法后缀名的字符串",
-	maxlength : $.format("请输入一个长度最多是 {0} 的字符串"),
-	minlength : $.format("请输入一个长度最少是 {0} 的字符串"),
-	rangelength : $.format("请输入一个长度介于 {0} 和 {1} 之间的字符串"),
-	range : $.format("请输入一个介于 {0} 和 {1} 之间的值"),
-	max : $.format("该项不能大于 {0}"),
-	min : $.format("该项不能小于 {0}"),
-	username : "只能输入字符、数字、中文、和 _ - @ 的组合",
-	path : "只能输入字符和数字的组合"
+	maxlength : $.format(lang['qingshuruyigechangduzuiduoshi']),
+	minlength : $.format(lang['qingshuruyigechangduzuishaoshi']),
+	rangelength : $.format(lang['qingshuruyigechangdujieyu']),
+	range : $.format(lang['qingshurujieyuzhi']),
+	max : $.format(lang['gaixiangbunengdayu']),
+	min : $.format(lang['gaixiangbunengxiaoyu']),
+	username : lang['yonghumingzhinengshuru'],
+	path : lang['lujingzhinengzhuru']
 });
 $.extend({
 	 confirm: function(callback,title,msg) {
-	 if(title==null){title="确认"};
-	 if(msg==null){msg="确认删除?"};
+	 if(title==null){title=lang['queren']};
+	 if(msg==null){msg=lang['querenshanchu']};
 	 $("BODY").append("<div id='dialog-message' title='"+title+"'><p>"+msg+"</p></div>");
 	 $("#dialog-message").dialog({
 				modal: true,
@@ -69,8 +69,8 @@ $.extend({
 		$("#dialog-message").dialog('open');
 	},
 	confirmToUrl: function(url,title,msg) {
-		if(title==null){title="确认"};
-		if(msg==null){msg="确认删除?"};
+		if(title==null){title=lang['queren']};
+		if(msg==null){msg=lang['querenshanchu']};
 		 $("BODY").append("<div id='dialog-message' title='"+title+"'><p>"+msg+"</p></div>");
 		 $("#dialog-message").dialog({
 					modal: true,
@@ -91,7 +91,7 @@ $.extend({
 		$("#dialog-message").dialog('open');
 	},
 	alert: function(title,msg) {  
-		 if(title==null){title="提示"};
+		 if(title==null){title=lang['tishi']};
 		 $("body").append("<div id='dialog-message' title='"+title+"'><p>"+msg+"</p></div>");
 		 $("#dialog-message").dialog({
 					modal: true,
